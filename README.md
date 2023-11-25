6c023c0836bca5767eef88875beac653ca63709d
Oleg Bilovus <68842946+OB-UNISA@users.noreply.github.com>
2023-11-25 13:45:46 +0100

    added the possibility to add extra data

--- README.md
+++ README.md
@@ -1,11 +1,24 @@
+```
+usage: ab to CSV [-h] (--inp INP | --stdin) --out OUT [-a] [--extra_data EXTRA_DATA]
+
+options:
+  -h, --help            show this help message and exit
+  --inp INP             Input file
+  --stdin               Input from stdin
+  --out OUT             Output file
+  -a                    Append to input file
+  --extra_data EXTRA_DATA
+                        Add extra data. Format is the same as in Ansible for --extra-vars: "k1=v1 k2=v2"
+```
+
 ## Apache Benchmark
 
 There is a Python script [abToCSV.py](abToCSV.py) which can be used to parse *ab*'s output to a CSV file.
 The script can be used with a pipe from *ab* to read the data and convert it to CSV when *ab* finishes.
 
-Example:
+#### Example:
 ```bash
-ab -n 50000 -c 200 http://127.0.0.1:8090/static-20k.html | python abToCSV.py --stdin --out out.csv -a
+ab -n 10000 -c 150 http://127.0.0.1:8090/static-20k.html | python abToCSV.py --stdin --out out.csv -a
 ```
 CSV file example:
 
@@ -16,14 +29,20 @@ CSV file example:
 |lighttpd/1.4.72|127.0.0.1|8092|/static-20k.html|20480|150|7.035|10000|0|1421.42|105.528|0.704|96|114|127|136|166|194|232|265|394|
 
 
-Usage:
-```bash
-usage: ab to CSV [-h] [--inp INP] --out OUT [-a] [--stdin]
+### Add extra data
+In case you need to add extra data such as the machine's specs, you can  use the option 
+`--extra_data` which follow the same format `"k1=v1 k2=v2"` as [Ansible's](https://docs.ansible.com/ansible/latest/playbook_guide/playbooks_variables.html#key-value-format) `--extra-vars`
 
-options:
-  -h --help  show this help message and exit
-  --inp INP   Input file
-  --out OUT   Output file
-  -a          Append to input file
-  --stdin     Input from stdin
+#### Example
+```bash
+ab -n 10000 -c 150 http://127.0.0.1:8090/static-20k.html | python abToCSV.py --stdin --out out.csv -a --extra_data "ram=2048 vcpu=2"
 ```
+CSV file example:
+
+|server|host|port|path|doc_len|conc|time|r_comp|r_fail|rps|tpr|tpr_all|50|66|75|80|90|95|98|99|100|ram|vcpu|
+|------|----|----|----|-------|----|----|------|------|---|---|-------|--|--|--|--|--|--|--|--|---|---|----|
+|Apache/2.4.57|127.0.0.1|8090|/static-20k.html|20480|150|14.709|10000|0|679.84|220.639|1.471|207|234|253|270|325|371|417|460|649|2048|2|
+
+This works also when using `-a`. If a csv header was previously defined, it will place the value in the correct column.
+
+
--- abToCSV.py
+++ abToCSV.py
@@ -8,66 +8,99 @@ def sep_strip(data, space=False, sep=':'):
     return res
 
 
-def ab_to_dict(input):
-    ab_dict = {}
-    ab_dict['server'] = sep_strip(input[0])
-    ab_dict['host'] = sep_strip(input[1])
-    ab_dict['port'] = sep_strip(input[2])
-    ab_dict['path'] = sep_strip(input[4])
-    ab_dict['doc_len'] = sep_strip(input[5], True)
-    ab_dict['conc'] = sep_strip(input[7])
-    ab_dict['time'] = sep_strip(input[8], True)
-    ab_dict['r_comp'] = sep_strip(input[9])
-    ab_dict['r_fail'] = sep_strip(input[10])
-    ab_dict['rps'] = sep_strip(input[13], True)
-    ab_dict['tpr'] = sep_strip(input[14], True)
-    ab_dict['tpr_all'] = sep_strip(input[15], True)
+def ab_to_dict(data):
+    ab_dict = {'server': sep_strip(data[0]),
+               'host': sep_strip(data[1]),
+               'port': sep_strip(data[2]),
+               'path': sep_strip(data[4]),
+               'doc_len': sep_strip(data[5], True),
+               'conc': sep_strip(data[7]),
+               'time': sep_strip(data[8], True),
+               'r_comp': sep_strip(data[9]),
+               'r_fail': sep_strip(data[10]),
+               'rps': sep_strip(data[13], True),
+               'tpr': sep_strip(data[14], True),
+               'tpr_all': sep_strip(data[15], True)}
 
     i = -1
-    for j in range(15, len(input)):
-        if '50%' in input[j]:
+    for j in range(15, len(data)):
+        if '50%' in data[j]:
             i = j
             break
 
     sep = '%'
-    ab_dict['50'] = sep_strip(input[i], sep=sep)
-    ab_dict['66'] = sep_strip(input[i + 1], sep=sep)
-    ab_dict['75'] = sep_strip(input[i + 2], sep=sep)
-    ab_dict['80'] = sep_strip(input[i + 3], sep=sep)
-    ab_dict['90'] = sep_strip(input[i + 4], sep=sep)
-    ab_dict['95'] = sep_strip(input[i + 5], sep=sep)
-    ab_dict['98'] = sep_strip(input[i + 6], sep=sep)
-    ab_dict['99'] = sep_strip(input[i + 7], sep=sep)
-    ab_dict['100'] = sep_strip(input[i + 8], True, sep=sep)
+    ab_dict['50'] = sep_strip(data[i], sep=sep)
+    ab_dict['66'] = sep_strip(data[i + 1], sep=sep)
+    ab_dict['75'] = sep_strip(data[i + 2], sep=sep)
+    ab_dict['80'] = sep_strip(data[i + 3], sep=sep)
+    ab_dict['90'] = sep_strip(data[i + 4], sep=sep)
+    ab_dict['95'] = sep_strip(data[i + 5], sep=sep)
+    ab_dict['98'] = sep_strip(data[i + 6], sep=sep)
+    ab_dict['99'] = sep_strip(data[i + 7], sep=sep)
+    ab_dict['100'] = sep_strip(data[i + 8], True, sep=sep)
 
     return ab_dict
+
+
+def extra_data(data):
+    values = data.split(' ')
+    dir_values = {}
+    for val in values:
+        k, v = val.split('=')
+        dir_values[k] = v
+
+    return dir_values
+
+
+def add_extra_data(e_data, out_csv):
+    out_csv[0] = out_csv[0][:-1]
+    out_csv[-1] = out_csv[-1][:-1]
+    headers = out_csv[0].split(',')
+    values = out_csv[-1].split(',')
+
+    for k, v in e_data.items():
+        try:
+            i = headers.index(k)
+        except ValueError:
+            i = len(headers)
+            headers.append(k)
+
+        len_values = len(values)
+        if i >= len_values:
+            values.extend(['' for _ in range(i - len_values + 1)])
+        values[i] = v
+
+    out_csv[0] = ','.join(headers) + '\n'
+    out_csv[-1] = ','.join(values)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser('ab to CSV')
     in_grp = parser.add_mutually_exclusive_group(required=True)
     in_grp.add_argument('--inp', help='Input file')
+    in_grp.add_argument('--stdin', action='store_true', help='Input from stdin')
     parser.add_argument('--out', required=True, help='Output file')
     parser.add_argument('-a', action='store_true', help='Append to input file')
-    in_grp.add_argument('--stdin', action='store_true', help='Input from stdin')
+    parser.add_argument('--extra_data', help='Add extra data. Format is the same as in Ansible for --extra-vars: '
+                                             '"k1=v1 k2=v2"')
 
     args = parser.parse_args()
 
     if args.stdin:
-        data = []
+        _data = []
         while True:
             try:
                 line = input()
-                data.append(line)
+                _data.append(line)
             except EOFError:
                 break
     else:
         with open(args.inp, 'r') as f:
-            data = f.readlines()
+            _data = f.readlines()
 
     mode = 'a' if args.a else 'w'
     with open(args.out, mode) as f:
-        abd = ab_to_dict(data[7:])
+        abd = ab_to_dict(_data[7:])
         csv_str = ''
         if not args.a:
             csv_str = ','.join(abd.keys())
@@ -75,3 +108,11 @@ if __name__ == '__main__':
         csv_str += ','.join(abd.values())
         csv_str += '\n'
         f.write(csv_str)
+
+    if args.extra_data:
+        with open(args.out, 'r+') as f:
+            f_data = f.readlines()
+            ex_data = extra_data(args.extra_data)
+            add_extra_data(ex_data, f_data)
+            f.seek(0)
+            f.write(''.join(f_data))
